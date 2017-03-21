3313f79e36884def1bab69f1a9e93982d830e1bb
Alan Zheng <AlanZheng@MacBook-Pro.local>
2017-03-21 16:51:05 +0800

    Initial commit

---
+++ app/src/main/java/com/tensun/serialtools/SerialPortReceiveParameters.java
@@ -0,0 +1,39 @@
+package com.tensun.serialtools;
+
+
+public class SerialPortReceiveParameters {
+
+    /**
+     * 与上次接收数据的间隔空闲时间，用于清除缓存
+     */
+    private int mFreeTime = 2000;
+
+    /**
+     * 过滤时间，多长时间内数据完全相同为不需要数据
+     */
+    private int mFilterTime = 3000;
+
+    public SerialPortReceiveParameters() {
+    }
+
+    public SerialPortReceiveParameters(int freeTime, int filterTime) {
+        mFreeTime = freeTime;
+        mFilterTime = filterTime;
+    }
+
+    public int getFreeTime() {
+        return mFreeTime;
+    }
+
+    public void setFreeTime(int freeTime) {
+        mFreeTime = freeTime;
+    }
+
+    public int getFilterTime() {
+        return mFilterTime;
+    }
+
+    public void setFilterTime(int filterTime) {
+        mFilterTime = filterTime;
+    }
+}
